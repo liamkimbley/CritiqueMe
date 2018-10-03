2a0791b5312b1fd876a87865d18773f76489271d
liamkimbley <liam.kimbley@gmail.com>
2018-10-03 11:56:45 -0600

    project updated

---
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.critique.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.critique.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	public User findByUsername(String name);
+	public User findByEmail(String email);
+	public User findOneUserByUsername(String name);
+
+}
