e015ae3c3b5e358f808915464c240b523b3758c2
liamkimbley <liam.kimbley@gmail.com>
2018-10-04 10:58:50 -0600

    project updated

--- CritiqueREST/src/main/java/com/skilldistillery/critique/services/PostServiceImpl.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/services/PostServiceImpl.java
@@ -2,6 +2,7 @@ package com.skilldistillery.critique.services;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,56 +12,102 @@ import com.skilldistillery.critique.repositories.PostRepository;
 
 @Service
 public class PostServiceImpl implements PostService {
-	
+
 	@Autowired
 	private PostRepository postRepo;
 
 	@Override
 	public List<Post> findPostsByCategoryId(int id) {
-		return null;
-		
+		return postRepo.queryForPostsByCategoryId(id);
+
 	}
 
 	@Override
 	public List<Post> findPostsByCreateDate(LocalDate date) {
-		return null;
-		
+		return postRepo.findByCreatedDate(date);
+
 	}
 
 	@Override
 	public List<Post> findAllPosts() {
-		return null;
-		
+		return postRepo.findAll();
+
 	}
 
 	@Override
 	public Post findPostById(int id) {
+		Post p = postRepo.queryForPostWithCommentsByPostId(id);
+
+		if (p != null) {
+			return p;
+		}
+
 		return null;
-		
 	}
 
 	@Override
 	public Post create(Post post) {
-		return null;
-		
+		Post p = new Post();
+
+		if (post != null) {
+			if (post.getContent() != null && !post.getContent().equals("")) {
+				p.setContent(post.getContent());
+			}
+			if (post.getProfile() != null) {
+				p.setProfile(post.getProfile());
+			}
+		}
+
+		return postRepo.saveAndFlush(p);
+
 	}
 
 	@Override
 	public Post update(int id, Post post) {
+		Optional<Post> op = postRepo.findById(id);
+
+		if (op.isPresent()) {
+			Post p = op.get();
+			if (post.getContent() != null && !post.getContent().equals("")) {
+				p.setContent(post.getContent());
+			}
+			if (post.getProfile() != null) {
+				p.setProfile(post.getProfile());
+			}
+			return postRepo.saveAndFlush(p);
+		}
 		return null;
-		
+
 	}
 
 	@Override
 	public Post replace(int id, Post post) {
+		Optional<Post> op = postRepo.findById(id);
+
+		if (op.isPresent()) {
+			Post p = op.get();
+			if (post.getContent() != null && !post.getContent().equals("")) {
+				p.setContent(post.getContent());
+			}
+			if (post.getProfile() != null) {
+				p.setProfile(post.getProfile());
+			}
+			return postRepo.saveAndFlush(p);
+		}
 		return null;
-		
+
 	}
 
 	@Override
 	public boolean delete(int id) {
+		try {
+			postRepo.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return false;
-		
 	}
 
 }
