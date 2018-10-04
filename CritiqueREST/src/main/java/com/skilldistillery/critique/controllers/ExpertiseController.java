b3dd99acc52835a473172dd7d169e63fa8bd6141
Samantha Janik <samajanik@gmail.com>
2018-10-04 11:43:25 -0600

    Expertise Controller and services implemented

--- CritiqueREST/src/main/java/com/skilldistillery/critique/controllers/ExpertiseController.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/controllers/ExpertiseController.java
@@ -1,8 +1,35 @@
 package com.skilldistillery.critique.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.critique.entities.Expertise;
+import com.skilldistillery.critique.services.ExpertiseService;
+
 @RestController
+@RequestMapping("api")
 public class ExpertiseController {
+	
+	@Autowired
+	private ExpertiseService exServ;
+	
+	@RequestMapping(path = "skills", method = RequestMethod.GET)
+	public List<Expertise> index(HttpServletRequest req, HttpServletResponse res) {
+		List<Expertise> skills = exServ.index();
+		if (skills != null) {
+			res.setStatus(200);
+			return skills;
+		} else {
+			res.setStatus(500);
+			return null;
+		}
+	}
 
 }
