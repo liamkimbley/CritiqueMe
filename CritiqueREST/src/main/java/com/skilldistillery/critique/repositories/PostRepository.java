8969616880a20845c1fe53b84a163ea8d358b720
liamkimbley <liam.kimbley@gmail.com>
2018-10-04 13:23:41 -0600

    project updated

--- CritiqueREST/src/main/java/com/skilldistillery/critique/repositories/PostRepository.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/repositories/PostRepository.java
@@ -14,7 +14,7 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 	public List<Post> findByProfile_Id(Integer id);
 	public List<Post> findByCreatedDate(LocalDate date);
 	@Query("SELECT p FROM Post p JOIN FETCH p.comments WHERE p.id = :id")
-	public Post queryForPostWithCommentsByPostId(Integer id);
+	public Post queryForPostWithCommentsByPostId(@Param ("id") Integer id);
 	
 	@Query("SELECT p FROM Post p, Category c WHERE  c.id = :cid")
 	public List<Post> queryForPostsByCategoryId(@Param ("cid") Integer cid);
