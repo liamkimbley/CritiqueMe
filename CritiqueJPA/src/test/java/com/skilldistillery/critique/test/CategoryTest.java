831fb2e23509178a5ded75baddac8aa359d02eb4
liamkimbley <liam.kimbley@gmail.com>
2018-10-04 09:03:43 -0600

    project updated

--- CritiqueJPA/src/test/java/com/skilldistillery/critique/test/CategoryTest.java
+++ CritiqueJPA/src/test/java/com/skilldistillery/critique/test/CategoryTest.java
@@ -1,38 +1,48 @@
 package com.skilldistillery.critique.test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.skilldistillery.critique.entities.Category;
 
 class CategoryTest {
+	private static EntityManagerFactory emf;
 	private EntityManager em;
-	private EntityManagerFactory emf;
 	private Category cat;
 	
+	@BeforeAll
+	public static void beforeTests() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CritiqueJPA");
+	}
+	
 	@BeforeEach
-	public void setup() {
-		emf = Persistence.createEntityManagerFactory("CritiqueMe");
+	public void setup () {
 		em = emf.createEntityManager();
 		cat = em.find(Category.class, 1);
+	}
+
+	@AfterAll
+	public static void afterTests() {
+		emf.close();
 	}
 	
 	@AfterEach
 	public void tearDown() {
 		em.close();
-		emf.close();
 	}
 	
 	@Test
-	void test_many_categories_can_be_in_one_post() {
-		fail("Not yet implemented");
+	void test_category_has_name() {
+		assertEquals("Music", cat.getName());
 	}
 
 }
