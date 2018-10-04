3895fd4f00b7135ec2f39f5a943cc8f799eb3cf5
Owen Harris <owenharris21@icloud.com>
2018-10-04 10:15:48 -0600

    10:15

--- CritiqueREST/src/main/java/com/skilldistillery/critique/services/UserService.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/services/UserService.java
@@ -1,5 +1,13 @@
 package com.skilldistillery.critique.services;
 
-public interface UserService {
+import java.util.List;
 
+import com.skilldistillery.critique.entities.User;
+
+public interface UserService {
+	public User findByUsername(String name);
+	public User findByEmail(String email);
+	public User findOneUserByUsername(String name);
+	public List<User> findByActive(Boolean active);
+	public List<User> findByRole(String role);
 }
--- CritiqueREST/src/main/java/com/skilldistillery/critique/services/UserServiceImpl.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/services/UserServiceImpl.java
@@ -1,8 +1,41 @@
 package com.skilldistillery.critique.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.critique.entities.User;
+import com.skilldistillery.critique.repositories.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public User findByUsername(String name) {
+		return userRepo.findByUsername(name);
+	}
+
+	@Override
+	public User findByEmail(String email) {
+		return userRepo.findByEmail(email);
+	}
+
+	@Override
+	public User findOneUserByUsername(String name) {
+		return userRepo.findOneUserByUsername(name);
+	}
+
+	@Override
+	public List<User> findByActive(Boolean active) {
+		return userRepo.findByActive(active);
+	}
+
+	@Override
+	public List<User> findByRole(String role) {
+		return userRepo.findByRole(role);
+	}
 
 }
