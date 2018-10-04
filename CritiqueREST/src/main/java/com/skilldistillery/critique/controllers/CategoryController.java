c26e24c9f66c0c3a5342e6346a787863a7aeb38d
liamkimbley <liam.kimbley@gmail.com>
2018-10-04 11:13:52 -0600

    project updated

--- CritiqueREST/src/main/java/com/skilldistillery/critique/controllers/CategoryController.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/controllers/CategoryController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.skilldistillery.critique.services.CategoryService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({"*", "http://localhost:4201"})
 public class CategoryController {
 	@Autowired
 	private CategoryService catServ;
--- CritiqueREST/src/main/java/com/skilldistillery/critique/controllers/UserController.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.skilldistillery.critique.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
+@CrossOrigin({"*", "http://localhost:4201"})
 public class UserController {
 
 }
