140c9a4593c61ce479acc5db24fb721e80d88f28
liamkimbley <liam.kimbley@gmail.com>
2018-10-04 11:11:41 -0600

    project updated

---
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/services/AuthService.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.critique.services;
+
+import com.skilldistillery.critique.entities.User;
+
+public interface AuthService {
+
+	public User register(String json);
+}
---
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/services/AuthServiceImpl.java
@@ -0,0 +1,53 @@
+package com.skilldistillery.critique.services;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skilldistillery.critique.entities.User;
+import com.skilldistillery.critique.repositories.UserRepository;
+
+@Repository
+@Transactional
+@Service
+public class AuthServiceImpl implements AuthService {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private PasswordEncoder encoder;
+
+	@Override
+	public User register(String json) {
+		ObjectMapper om = new ObjectMapper();
+		User user = null;
+
+		try {
+			user = om.readValue(json, User.class);
+
+			String encodedPW = encoder.encode(user.getPassword());
+			user.setPassword(encodedPW); // only persist encoded password
+
+			// set other fields to default values
+			user.setActive(true);
+			user.setRole("standard");
+
+			userRepo.saveAndFlush(user);
+		} catch (Exception e) {
+			System.out.println(e);
+		}		
+		return user;
+	}
+
+}
+
