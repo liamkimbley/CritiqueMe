870b869cf31147fb7029c5e5371665f4f907decd
Samantha Janik <samajanik@gmail.com>
2018-10-04 10:40:50 -0600

    commented out query in VoteRepository until Vote Entity is completed

--- CritiqueREST/src/main/java/com/skilldistillery/critique/repositories/VoteRepository.java
+++ CritiqueREST/src/main/java/com/skilldistillery/critique/repositories/VoteRepository.java
@@ -8,6 +8,6 @@ import com.skilldistillery.critique.entities.Vote;
 
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
 
-	@Query("SELECT v FROM Vote v WHERE v.comment.id = :cid AND v.profile.id = :pid")
-	public Vote findByCommentIdAndProfileId(@Param ("cid") Integer cid, @Param ("pid") Integer pid);
+//	@Query("SELECT v FROM Vote v WHERE v.comment.id = :cid AND v.profile.id = :pid")
+//	public Vote findByCommentIdAndProfileId(@Param ("cid") Integer cid, @Param ("pid") Integer pid);
 }
