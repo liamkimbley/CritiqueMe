82dfa37a6147b8219bdb3219f9ad08e504c49142
liamkimbley <liam.kimbley@gmail.com>
2018-10-05 14:45:13 -0600

    project updated

--- CritiqueAngular/critique/src/app/models/profile.ts
+++ CritiqueAngular/critique/src/app/models/profile.ts
@@ -1,15 +1,21 @@
+import { Location } from './location';
+import { User } from './user';
 export class Profile {
   id: number;
   firstName: string;
   lastName: string;
   bio: string;
   image: string;
+  loc: Location;
+  user: User;
 
-  constructor(id?: number, firstName?: string, lastName?: string, bio?: string, image?: string) {
+  constructor(id?: number, firstName?: string, lastName?: string, bio?: string, image?: string, loc?: Location, user?: User) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.bio = bio;
     this.image = image;
+    this.loc = loc;
+    this.user = user;
   }
 }
