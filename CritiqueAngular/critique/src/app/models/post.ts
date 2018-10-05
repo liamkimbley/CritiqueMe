9049012fc30c3199f20e886db465a6857c2b10bb
liamkimbley <liam.kimbley@gmail.com>
2018-10-05 09:00:35 -0600

    project updated

--- CritiqueAngular/critique/src/app/models/post.ts
+++ CritiqueAngular/critique/src/app/models/post.ts
@@ -3,11 +3,15 @@ export class Post {
   content: string;
   createdDate: string;
   updatedDate: string;
+  title: string;
+  media: string;
 
-  constructor(id?: number, content?: string, createdDate?: string, updatedDate?: string) {
+  constructor(id?: number, title?: string, media?: string, content?: string, createdDate?: string, updatedDate?: string) {
     this.id = id;
     this.content = content;
     this.createdDate = createdDate;
     this.updatedDate = updatedDate;
+    this.title = title;
+    this.media = media;
   }
 }
--- CritiqueAngular/critique/src/app/models/profile.ts
+++ CritiqueAngular/critique/src/app/models/profile.ts
@@ -3,11 +3,13 @@ export class Profile {
   firstName: string;
   lastName: string;
   bio: string;
+  image: string;
 
-  constructor(id?: number, firstName?: string, lastName?: string, bio?: string) {
+  constructor(id?: number, firstName?: string, lastName?: string, bio?: string, image?: string) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.bio = bio;
+    this.image = image;
   }
 }
