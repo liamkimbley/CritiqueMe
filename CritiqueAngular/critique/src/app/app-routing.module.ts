dd374dc009d42836422dfc5398f5bf486c8289b1
Samantha Janik <samajanik@gmail.com>
2018-10-05 16:58:50 -0600

    added a lot of paths in app routing

--- CritiqueAngular/critique/src/app/app-routing.module.ts
+++ CritiqueAngular/critique/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CommentComponent } from './comment/comment.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -14,6 +15,23 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'post', component: PostComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'profile/:id', component: ProfileComponent },
+  { path: 'profile/firstname/:firstname', component: ProfileComponent },
+  { path: 'profile/lastname/:lastname', component: ProfileComponent },
+  { path: 'profile/fullname/:firstname/:lastname', component: ProfileComponent },
+  { path: 'profile/city/:city', component: ProfileComponent },
+  { path: 'profile/state/:state', component: ProfileComponent },
+  { path: 'profile/country/:country', component: ProfileComponent },
+  { path: 'profile/user/:username', component: ProfileComponent },
+  { path: 'categories', component: PostComponent },
+  { path: 'categories/:id/posts', component: PostComponent },
+  { path: 'posts/:id', component: PostComponent },
+  { path: 'posts/title/:title', component: PostComponent },
+  { path: 'posts/:id/comments', component: CommentComponent },
+  { path: 'comments/:id', component: CommentComponent },
+  { path: 'comments/:cid/votes', component: CommentComponent },
+  { path: 'comments/:cid/vote/:vid', component: CommentComponent },
+  { path: 'comments/:cid/vote', component: CommentComponent }
   // { path: '**', component: NotFoundComponent }
   // make a not found component later
 ];
