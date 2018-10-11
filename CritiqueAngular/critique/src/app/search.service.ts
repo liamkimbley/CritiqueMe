35e11ff9e95028916e5e939769e9017fe031d590
Samantha Janik <samajanik@gmail.com>
2018-10-11 16:56:49 -0600

    fixed search to not display when not logged in

--- CritiqueAngular/critique/src/app/search.service.ts
+++ CritiqueAngular/critique/src/app/search.service.ts
@@ -22,42 +22,83 @@ export class SearchService {
     private http: HttpClient,
     private auth: AuthService,
     private router: Router
-  ) { }
+  ) {}
 
   public getProfilesByFirstName(firstName: String): Observable<Profile[]> {
-    return this.http.get<Profile[]>(this.urlProfile + '/firstname/' + firstName).pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError('Error retrieving PROFILES: ' + 'Status: ' + err);
-      })
-    );
+    if (this.auth.checkLogin()) {
+      const headers = new HttpHeaders().set(
+        'Authorization',
+        `Basic ${this.auth.getToken()}`
+      );
+      return this.http
+        .get<Profile[]>(this.urlProfile + '/firstname/' + firstName, {
+          headers
+        })
+        .pipe(
+          catchError((err: any) => {
+            console.log(err);
+            return throwError('Error retrieving PROFILES: ' + 'Status: ' + err);
+          })
+        );
+    } else {
+      this.router.navigateByUrl('login');
+    }
   }
 
   public getProfilesByLastName(lastName: String): Observable<Profile[]> {
-    return this.http.get<Profile[]>(this.urlProfile + '/lastname/' + lastName).pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError('Error retrieving PROFILES: ' + 'Status: ' + err);
-      })
-    );
+    if (this.auth.checkLogin()) {
+      const headers = new HttpHeaders().set(
+        'Authorization',
+        `Basic ${this.auth.getToken()}`
+      );
+      return this.http
+        .get<Profile[]>(this.urlProfile + '/lastname/' + lastName, { headers })
+        .pipe(
+          catchError((err: any) => {
+            console.log(err);
+            return throwError('Error retrieving PROFILES: ' + 'Status: ' + err);
+          })
+        );
+    } else {
+      this.router.navigateByUrl('login');
+    }
   }
 
   public getProfilesByUsername(username: String): Observable<Profile[]> {
-    return this.http.get<Profile[]>(this.urlProfile + '/user/' + username).pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError('Error retrieving PROFILES: ' + 'Status: ' + err);
-      })
-    );
+    if (this.auth.checkLogin()) {
+      const headers = new HttpHeaders().set(
+        'Authorization',
+        `Basic ${this.auth.getToken()}`
+      );
+      return this.http
+        .get<Profile[]>(this.urlProfile + '/user/' + username, { headers })
+        .pipe(
+          catchError((err: any) => {
+            console.log(err);
+            return throwError('Error retrieving PROFILES: ' + 'Status: ' + err);
+          })
+        );
+    } else {
+      this.router.navigateByUrl('login');
+    }
   }
 
   public getPostsByTitle(title: String): Observable<Post[]> {
-    return this.http.get<Post[]>(this.urlPost + '/title/' + title).pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError('Error retrieving POSTS: ' + 'Status: ' + err);
-      })
-    );
+    if (this.auth.checkLogin()) {
+      const headers = new HttpHeaders().set(
+        'Authorization',
+        `Basic ${this.auth.getToken()}`
+      );
+      return this.http
+        .get<Post[]>(this.urlPost + '/title/' + title, { headers })
+        .pipe(
+          catchError((err: any) => {
+            console.log(err);
+            return throwError('Error retrieving POSTS: ' + 'Status: ' + err);
+          })
+        );
+    } else {
+      this.router.navigateByUrl('login');
+    }
   }
-
 }
