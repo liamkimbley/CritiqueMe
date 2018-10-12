95a35d77a72af1813139ac84cd74c9dc4dfaebd3
liamkimbley <liam.kimbley@gmail.com>
2018-10-12 15:50:35 -0600

    project updated

--- CritiqueAngular/critique/src/app/navigation/navigation.component.ts
+++ CritiqueAngular/critique/src/app/navigation/navigation.component.ts
@@ -11,7 +11,7 @@ export class NavigationComponent implements OnInit {
   navBarCheckLogin() {
     return this.authService.checkLogin();
   }
-  constructor(private authService: AuthService, router: Router) {}
+  constructor(private authService: AuthService, public router: Router) {}
 
   ngOnInit() {}
 }
